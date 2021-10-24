d153b404417059665c1205366da5ea473345a2b8
heenabhatti <40538700+heenabhatti@users.noreply.github.com>
2021-10-24 17:03:13 +0530

    Commit_1

---
+++ GitHubPractice/src/GitPractice1.java
@@ -0,0 +1,13 @@
+
+public class GitPractice1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello this is my first practice for Git and Git Hub"); //first changes 
+		
+
+	}
+
+}
